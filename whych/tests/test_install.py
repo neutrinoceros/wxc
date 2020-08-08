efdbe7bbc695076c3b1f7adb35fc6aa77f554aa0
Clément Robert <cr52@protonmail.com>
2020-08-08 15:50:51 +0200

    skip install tests on windows

--- whych/tests/test_install.py
+++ whych/tests/test_install.py
@@ -1,14 +1,19 @@
 import os
+import sys
 import tempfile
+
+import pytest
 
 from whych._installer import _lookup_rcfile, main
 
 
+@pytest.mark.skipif(sys.platform.startswith("win"), reason="no windows support")
 def test_lookup_rcfile():
     rcfile = _lookup_rcfile(interactive=False)
     assert rcfile.is_file()
 
 
+@pytest.mark.skipif(sys.platform.startswith("win"), reason="no windows support")
 def test_install():
     with tempfile.TemporaryDirectory() as tmpdir:
         main(install_dir=os.path.join(tmpdir, ".app"))
