953a52a44cfb35082f940c713693d274f0e79bd1
Clément Robert <cr52@protonmail.com>
2020-08-09 20:37:28 +0200

    cover default dir install

--- whych/tests/test_install.py
+++ whych/tests/test_install.py
@@ -23,14 +23,28 @@ def test_lookup_rcfile():
 
 
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="no windows support")
-def test_install():
-
+def test_default_install():
     with tempfile.TemporaryDirectory() as tmpdir:
+        fake_default_dir = os.path.join(tmpdir, ".app")
+        with mock.patch.object(
+            _installer, "DEFAULT_INSTALL_DIR", fake_default_dir
+        ):
+            with tempfile.NamedTemporaryFile(dir=str(Path.home())) as tmprcfile:
+                with mock.patch.object(
+                    _installer, "RCFILE_LOOKUP_TABLE", [tmprcfile.name]
+                ):
+                    assert not Path(fake_default_dir).exists()
+                    main()
+
+
+@pytest.mark.skipif(sys.platform.startswith("win"), reason="no windows support")
+def test_repeated_install():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        install_dir = os.path.join(tmpdir, ".app")
         with tempfile.NamedTemporaryFile(dir=str(Path.home())) as tmprcfile:
             with mock.patch.object(
                 _installer, "RCFILE_LOOKUP_TABLE", [tmprcfile.name]
             ):
-                install_dir = os.path.join(tmpdir, ".app")
                 main(install_dir=install_dir)
                 with pytest.raises(SystemExit):
                     main(install_dir=install_dir)
