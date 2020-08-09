cb47590863a9a6c804f4001750aeff3288af955c
Clément Robert <cr52@protonmail.com>
2020-08-09 20:00:29 +0200

    add a test for unfindable rcfile

--- whych/tests/test_install.py
+++ whych/tests/test_install.py
@@ -14,6 +14,13 @@ def test_lookup_rcfile():
     rcfile = _lookup_rcfile(interactive=False)
     assert rcfile.is_file()
 
+    # mock a non-existing file by creating a tempfile and removing it
+    with tempfile.NamedTemporaryFile(dir=str(Path.home())) as tmprcfile:
+        filename = tmprcfile.name
+    with mock.patch.object(_installer, "RCFILE_LOOKUP_TABLE", [filename]):
+        with pytest.raises(FileNotFoundError):
+            _lookup_rcfile(interactive=False)
+
 
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="no windows support")
 def test_install():
